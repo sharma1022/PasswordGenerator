6a7ff3ef9a8e9aa6ceba20db3335410aeb6ae7af
sharma1022 <samsharma.dev@gmail.com>
2024-12-09 17:24:30 -0800

    created basic schema for the password

--- App.tsx
+++ App.tsx
@@ -1,12 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import * as Yup from 'yup';
+
+const passwordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be min 4 characters')
+    .max(16, 'Should be max 16 characters')
+    .required('Length is required'),
+});
 
 export default function App() {
+  const [password, setPassword] = useState('');
   return (
     <View>
       <Text>App</Text>
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
